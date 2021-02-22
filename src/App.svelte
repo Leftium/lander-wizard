ceddcae433f29a926b9d6121cc380c7286ad1ae7
Leftium <john@leftium.com>
2021-02-22 17:47:25 +0900

    Start over-all wizard structure.

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,9 @@
 <script lang="coffeescript">
-  export name = null
+  import ProgressNav  from './components/ProgressNav.svelte'
 
-  ## Counter sample based on https://svelte.dev/tutorial/reactive-statements
+  fileInput = null
+
+    ## Counter sample based on https://svelte.dev/tutorial/reactive-statements
   count = 0
  
   ## Mark Svelte reactive CoffeeScript statements by escaping out to JavaScript.
@@ -13,40 +15,130 @@
     count = 9
   `}`     ## End reactive block.
 
-  handleClick = () ->
-    count += 1
+  openImagePicker = () ->
+    fileInput.click()
 </script>
 
 <template lang="pug">
-  main
-    h1 Hello {name}!
-    p.
-      Visit the #[a(href='https://svelte.dev/tutorial') Svelte tutorial]
-      to learn how to build Svelte apps.
+  div.nav: ProgressNav
+    ul
+      li: a(href='#image') Image
+      li: a(href='#headline') Headline
+      li: a(href='#body') Body
+        li: a(href='#expert') Expert and Problem
+        li: a(href='#solution') Solution and Common Enemy
 
-    // How reactive buttons are done in Svelte + Pug.
-    // Note CoffeeScript cannot be used here.
-    button(on:click='{handleClick}') Clicked {count} {count === 1 ? 'time' : 'times'}
+  main
+    fieldset#image
+      legend Image
+      div
+          img(src='img/ad-image.jpg' alt='ad image' on:click='{openImagePicker}')
+          button(on:click='{openImagePicker}') Upload Image
+          input(type='file' bind:this='{fileInput}' style='display:none')
+
+      div.ai: :markdown-it(linkify langPrefix='highlight-')
+        ## AI Analysis
+         - **Too much text.** Facebook rejects ad images with too much text.
+           Even if not rejected, the ad will be given fewer impressions.
+           Limit text to 25% of image.
+         - **Negative sentiment.** Facebook prefers images of happy people.
+         - **Lone person.** Facebook prefers images with two or more people together.
+      div.guide: :markdown-it(linkify langPrefix='highlight-')
+        ## Does your ad image satisfy these properties of winning images?
+        - **Good images get the user to stop scrolling.** People scan the images in their newsfeed,
+          only reading the few things that catch their attention. 
+          If you have a poor ad image, the rest of your ad doesn't matter because it won't even be read.
+          Good images stand out from other content in the Facebook newsfeed (think: photos and cartoons).
+          So vector-style images tend to stand out. Also experiment with colors: black and white,
+          grayscale, or super-colorful.
+
+        - **Good images get the user to ask a question in their head.**
+           The image should invoke a burning question in the user's mind that 
+          *must* be answered. When the viewer sees your ad image, they should be thinking something like:         
+          - What the heck is that?
+          - Where do I squeeze to lose weight?
+
+        - **Good images tell a story, but only *half* the story.** This is called opening a "loop." 
+        One common mistake is placing a product in the image (like a bottle). This closes the loop and
+        makes people think they're being sold something. Make sure the story is simple and easy to follow.
+
+        Get more training on winning images here.
+    fieldset#headline
+      legend Headline
+      div: input(value='This "7 Second Hack" Could Transform Your Health')
+      div
+        b Template: 
+        span Discover How This **[MECHANISM]** Could [BENEFIT]
+      div
+        b Models: 
+        ul 
+          li This "7 Second Hack" Could Transform Your Health
+          li This "7 Second Hack" Could Transform Your Health
+          li This "7 Second Hack" Could Transform Your Health
+
+    fieldset#body
+      legend Body
+      fieldset#expert
+        legend Expert and Problem
+        div Whistle-blower Dr. Chen discovered a simple method to lose weight and keep it off (not exercise or diet).
+      fieldset#solution
+        legend Solution and Common Enemy
+        div Whistle-blower Dr. Chen discovered a simple method to lose weight and keep it off (not exercise or diet).
+
+
+    footer
 </template>
 
 <style>
+  :global(.nav) {
+    position: fixed;
+    left: 1em;
+    top: 1em;
+    width: 14em;
+    line-height: 2;
+  }
   main {
-    text-align: center;
     padding: 1em;
     max-width: 240px;
     margin: 0 auto;
+    margin-left: 20em;
   }
 
-  h1 {
-    color: #ff3e00;
+  fieldset {
+    margin-bottom: 2em;
+  }
+  legend {
+    font-size: 300%;
     text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
+  }
+
+  fieldset fieldset legend {
+    font-size: 150%;
+  }
+
+  .ai {
+    background-color: aliceblue;
+  }
+
+  .guide {
+    background-color: lightyellow;
   }
 
   @media (min-width: 640px) {
     main {
       max-width: none;
     }
+  }
+
+  img {
+    width: 400px;
+  }
+
+  input {
+    width: 100%;
+  }
+
+  footer {
+    height: 1200px;
   }
 </style>
---
+++ src/components/ProgressNav.svelte
@@ -0,0 +1,190 @@
+<script type="text/coffeescript">
+# Progress Nav based on https://lab.hakim.se/progress-nav/
+
+import { onMount, tick } from 'svelte'
+
+toc = null
+tocPath = null
+tocItems = []
+
+# Factor of screen size that the element must cross
+# before it's considered visible
+TOP_MARGIN = 0
+BOTTOM_MARGIN = 0
+
+pathLength = null
+
+drawPath = () ->
+    tocItems = [].slice.call( toc.querySelectorAll( 'li' ) )
+
+
+    # Cache element references and measurements
+    tocItems = tocItems.map ( item ) ->
+        anchor = item.querySelector( 'a' )
+        target = document.getElementById( anchor.getAttribute( 'href' ).replace(/.*#/, '') )
+
+        return object =
+            listItem: item
+            anchor: anchor
+            target: target
+
+    # Remove missing targets
+    tocItems = tocItems.filter ( item ) ->
+        if item.target.getAttribute('show') is 'hidden'
+            return false
+        return !!item.target
+
+    path = []
+    pathIndent = null
+
+    tocItems.forEach ( item, i ) ->
+
+        x = item.anchor.offsetLeft - 5
+        y = item.anchor.offsetTop
+        height = item.anchor.offsetHeight
+
+        if i is 0
+            path.push( 'M', x, y, 'L', x, y + height )
+            item.pathStart = tocPath.getTotalLength() or 0
+        else
+             # Draw an additional line when there's a change in
+             # indent levels
+             if pathIndent isnt x then path.push( 'L', pathIndent, y )
+
+             path.push( 'L', x, y )
+
+             # Set the current path so that we can measure it
+             tocPath.setAttribute( 'd', path.join( ' ' ) )
+             item.pathStart = tocPath.getTotalLength() or 0
+
+             path.push( 'L', x, y + height )
+
+        pathIndent = x
+
+        tocPath.setAttribute( 'd', path.join( ' ' ) )
+        item.pathEnd = tocPath.getTotalLength()
+
+    pathLength = tocPath.getTotalLength()
+
+    sync()
+
+
+
+sync = () ->
+    if not tocItems.length then return
+
+    windowHeight = window.innerHeight
+
+    pathStart = Number.MAX_VALUE
+    pathEnd = 0
+
+    visibleItems = 0
+    tocItems.forEach ( item, i ) ->
+
+        {top, bottom} = item.target.getBoundingClientRect()
+
+        if nextItem = tocItems[i + 1]
+            bottom = nextItem.target.getBoundingClientRect().top
+        else
+            bottom = Infinity
+
+        if (bottom > windowHeight * TOP_MARGIN) and (top < windowHeight * ( 1 - BOTTOM_MARGIN ) )
+            pathStart = Math.min( item.pathStart, pathStart )
+            pathEnd = Math.max( item.pathEnd, pathEnd )
+
+            visibleItems += 1
+
+            item.listItem.classList.add( 'visible' )
+        else
+            item.listItem.classList.remove( 'visible' )
+
+      # Specify the visible path or hide the path altogether
+      # if there are no visible items
+      if visibleItems > 0 and (pathStart < pathEnd )
+          tocPath.setAttribute( 'stroke-dashoffset', '1' )
+          tocPath.setAttribute( 'stroke-dasharray', "1, #{pathStart}, #{pathEnd - pathStart}, #{pathLength}" )
+          tocPath.setAttribute( 'opacity', 1 )
+      else
+          tocPath.setAttribute( 'opacity', 0 );
+
+
+
+onMount () ->
+    # Wait for contents to be rendered first so we can find the id's of
+    # of elements with jump links/
+    await tick()
+
+    drawPath()
+
+</script>
+
+<svelte:window on:resize={drawPath} on:scroll={sync} />
+
+<nav class="toc" bind:this={toc}>
+    <slot />
+</nav>
+<svg
+    class="toc-marker"
+    width="200"
+    height="200"
+    xmlns="http://www.w3.org/2000/svg"
+>
+    <path
+        bind:this={tocPath}
+        stroke="#444"
+        stroke-width="3"
+        fill="transparent"
+        stroke-dasharray="0, 0, 0, 1000"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        transform="translate(-0.5, -0.5)"
+    />
+</svg>
+
+<style global>
+    .toc {
+        left: 3em;
+        top: 5em;
+        padding: 1em;
+        width: 14em;
+        line-height: 2;
+    }
+
+    :global(.toc ul) {
+        list-style: none;
+        padding: 0;
+        margin: 0;
+    }
+
+    :global(.toc ul ul) {
+        padding-left: 2em;
+    }
+
+    :global(.toc li a) {
+        display: inline-block;
+        color: #aaa;
+        text-decoration: none;
+        -webkit-transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
+        transition: all 0.3s cubic-bezier(0.23, 1, 0.32, 1);
+    }
+
+    :global(.toc li.visible > a) {
+        color: #111;
+        -webkit-transform: translate(5px);
+        transform: translate(5px);
+    }
+
+    :global(.toc-marker) {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: -1;
+    }
+
+    :global(.toc-marker path) {
+        -webkit-transition: all 0.3s ease;
+        transition: all 0.3s ease;
+    }
+</style>
